694b4fa220b037574d7b648d0e5edeef42150d09
Nguyễn Khoa <khoacode1305@gmail.com>
2024-10-15 12:45:33 -0700

    create a link to Login page

--- src/app/Root.js
+++ src/app/Root.js
@@ -1,8 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 function Root() {
     return (
         <>
             <p>This is Root page 1</p>
+            <ul>
+                <li><NavLink to="login">Login</NavLink></li>
+            </ul>
             <Outlet />
 
         </>
