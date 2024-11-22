260b4be5439473cb55c16cdc3d608f43750f44c3
Nguyễn Khoa <khoacode1305@gmail.com>
2024-11-22 15:54:27 -0700

    fix organize application pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Login from './app/Login';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import Login from './app/homepage/Login';
 import Root from './app/Root';
 //import Authen from './app/Authen';
-import SignUp from './app/SignUp';
-import {User, UserHomePage} from './app/User';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import HomePage from './app/Homepage';
+import SignUp from './app/homepage/SignUp';
+import User from './app/user/User';
+import UserHomepage from './app/user/UserHomepage';
+import HomePage from './app/homepage/Homepage';
+import AboutUs from './app/homepage/AboutUs';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path="ToDoList" element={<Root/>}>
     <Route path="homepage" element={<HomePage/>}>
+      <Route path="aboutUs" element={<AboutUs/>}/>
       <Route path="login" element={<Login/>}/>
-      <Route path="signup" element={<SignUp/>}/>
+      <Route path="signUp" element={<SignUp/>}/>
     </Route>
     <Route path="user" element={<User/>}>
-      <Route path=":username" element={<UserHomePage/>}/>
+      <Route path=":username" element={<UserHomepage/>}/>
     </Route>
   </Route>
 ));
