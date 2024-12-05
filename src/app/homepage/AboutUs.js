71924255e5b88f6ae0cd3c7fc207b1370cd14c18
Nguyễn Khoa <khoacode1305@gmail.com>
2024-12-04 22:36:58 -0700

    feat: create style for login page

--- src/app/homepage/AboutUs.js
+++ src/app/homepage/AboutUs.js
@@ -7,6 +7,8 @@ function AboutUs() {
                 <title>About Us | ToDo List</title>
             </Helmet>
             <p>About us</p>
+            <p>Too many tasks to handle?</p>
+            <p>Sign up now to relieve the stress of managing them</p>
         </>
     );
 };
